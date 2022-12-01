0f6946215ad0bab16ca762c85876dfc9b42d5194
Adam hakansson <adam97.live.com@live.com>
2022-12-01 21:06:54 +0100

    Add new 1 liner with single variable name variables

--- day-1.mjs
+++ day-1.mjs
@@ -12,10 +12,12 @@ import fs from "fs/promises";
 //   )
 //   .sort((a, b) => b - a);
 
-const mostCaloriesSorted = (await fs.readFile("day-1-data", "utf-8")).split("\r\n").reduce((total, curr) => curr.length === 0 ? total.concat([0]) : total.map((value, index) => index + 1 === total.length ? value + parseInt(curr) : value) , []).sort((a, b) => b - a);
+// const mostCaloriesSorted = (await fs.readFile("day-1-data", "utf-8")).split("\r\n").reduce((total, curr) => curr.length === 0 ? total.concat([0]) : total.map((value, index) => index + 1 === total.length ? value + parseInt(curr) : value) , []).sort((a, b) => b - a);
 
-console.log(`most calories: ${mostCaloriesSorted[0]}`);
+const a = (await fs.readFile("day-1-data", "utf-8")).split("\r\n").reduce((b, c) => c.length === 0 ? b.concat([0]) : b.map((d, e) => e + 1 === b.length ? d + parseInt(c) : d) , []).sort((f, g) => g - f);
+
+console.log(`most calories: ${a[0]}`);
 
 console.log(
-  `Calories in total of the top 3 elves: ${mostCaloriesSorted[0] + mostCaloriesSorted[1] + mostCaloriesSorted[2]}`
+  `Calories in total of the top 3 elves: ${a[0] + a[1] + a[2]}`
 );
