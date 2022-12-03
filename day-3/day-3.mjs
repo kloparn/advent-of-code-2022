442101452d0739e4e9211246cb5657e67011ae09
Adam hakansson <adam97.live.com@live.com>
2022-12-03 17:21:25 +0100

    Refactor code to be much faster and simpler

--- day-3/day-3.mjs
+++ day-3/day-3.mjs
@@ -3,7 +3,7 @@ import fs from "fs";
 const rugsacks = fs.readFileSync("day-3-data", "utf-8").split(/\r?\n/);
 // const rugsacks = fs.readFileSync("example-data", "utf-8").split(/\r?\n/);
 
-const alphabet = new Array().concat([..."abcdefghijklmnopqrstuvwxyz"], [..."ABCDEFGHIJKLMNOPQRSTUVWXYZ"]);
+const alphabet = [..."abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 
 function part1() {
   let priorityValue = 0;
@@ -12,14 +12,9 @@ function part1() {
     const firstHalf = wholeRugsack.slice(0, Math.ceil(wholeRugsack.length / 2)).split("");
     const secondHalf = wholeRugsack.slice(Math.ceil(wholeRugsack.length / 2)).split("");
 
-    const sharedCompartments = firstHalf.reduce(
-      (contains, curr) => (secondHalf.includes(curr) ? [...new Set([...contains, curr])] : contains),
-      []
-    );
+    const item = firstHalf.find((char) => secondHalf.includes(char));
 
-    for (const item of sharedCompartments) {
-      priorityValue += alphabet.indexOf(item) + 1;
-    }
+    priorityValue += alphabet.indexOf(item) + 1;
   }
 
   return priorityValue;
@@ -31,21 +26,11 @@ function part2() {
   for (let i = 0; i < rugsacks.length; i += 3) {
     const groupRugsack = [rugsacks[i], rugsacks[i + 1], rugsacks[i + 2]];
 
-    let sharedCompartments = [];
+    const item = groupRugsack[0]
+      .split("")
+      .find((char) => groupRugsack[1].includes(char) && groupRugsack[2].includes(char));
 
-    for (let index = 0; index < 3; index++) {
-      for (const char of groupRugsack[index]) {
-        if (groupRugsack[(i + 1) % 3].includes(char) && groupRugsack[(i + 2) % 3].includes(char)) {
-          sharedCompartments.push(char);
-        }
-      }
-    }
-
-    sharedCompartments = [...new Set(...sharedCompartments)];
-
-    for (const item of sharedCompartments) {
-      priorityValue += alphabet.indexOf(item) + 1;
-    }
+    priorityValue += alphabet.indexOf(item) + 1;
   }
 
   return priorityValue;
